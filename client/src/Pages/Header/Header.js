2e22f43ff105b75f59ef0d2ea8e986921765c48b
AmreetKumarkhuntia <79908976+AmreetKumarkhuntia@users.noreply.github.com>
2022-01-27 21:04:57 +0530

    Header Update

--- client/src/Pages/Header/Header.js
+++ client/src/Pages/Header/Header.js
@@ -4,9 +4,17 @@ const Header = () => {
     return ( 
         <>
         <header>
-            <h2>HAPI</h2>
+            <h3>HAPI</h3>
             <div className="Links">
+                <ul>
+                    <li>link 1</li>
+                    <li>link 2</li>
+                    <li>link 3</li>
+                    <li>link 4</li>
+                    <li>link 5</li>
+                </ul>
             </div>
+            <h3>Admin</h3>
         </header>
         </>
      );
