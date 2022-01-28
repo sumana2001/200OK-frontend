1b464184ed52fa977b878e5b6fd114c6ec9a5774
Rushikesh Kantule <73731585+rushi19383@users.noreply.github.com>
2022-01-28 11:24:27 +0530

    body update

--- client/src/Pages/Body/Body.js
+++ client/src/Pages/Body/Body.js
@@ -1,10 +1,32 @@
 import './css/Body.css';
 
-const Body = () => {
-    return ( 
-        <div className="body">
-        This will be our body
 
+const Body = () => {
+  
+    return ( 
+         
+        <div className="body">
+         
+         <h2>Search Hospitals:</h2>
+
+         <form>
+   <label>State       :
+        <input type="text" name="State" />
+      </label><br/>
+  <label>
+    District:
+    <input type="text" name="District" />
+  </label><br/>
+  <label>
+    Pincode:
+    <input type="text" name="Pincode" />
+  </label><br/>
+
+  <input type="submit" value="Search" />
+</form>
+ 
+        
+<hr></hr>
         </div>
     );
 }
